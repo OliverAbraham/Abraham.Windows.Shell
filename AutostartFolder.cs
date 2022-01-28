3ac4e94b9925dab6a3e347201c50d7541a157fb3
Oliver Abraham <mail@oliver-abraham.de>
2022-01-28 18:18:45 +0100

    Projektdateien hinzufügen.

---
+++ AutostartFolder.cs
@@ -0,0 +1,163 @@
+﻿// Using IWshRuntimeLibrary requires a COM reference to 'Windows Script Host Object Model'
+// ATTENTION: change the property "Embed Interop-types" to false!!!
+using IWshRuntimeLibrary;
+
+// Using Shell32 requires a COM reference to 'Microsoft Shell Controls and Automation'
+// ATTENTION: change the property "Embed Interop-types" to false!!!
+using Shell32;
+
+using Path = System.IO.Path;
+using System.IO;
+
+
+namespace Abraham.Windows.Shell
+{
+    /// <summary>
+    /// Creates and removes links to your program in the Windows Autostart folder
+    /// </summary>
+    ///
+    /// <remarks>
+    /// Oliver Abraham, www.oliver-abraham.de, mail@oliver-abraham.de
+    /// </remarks>
+    ///
+    /// <remarks>
+    /// ATTENTION: 
+    /// To compile this code, you need to add two references to your project:
+    /// Go to 'add reference" and COM --> Type libraries and add:
+    ///   1) 'Windows Script Host Object Model'
+    ///   2) 'Microsoft Shell Controls and Automation'
+    ///
+    /// Check that you see in your project:
+    ///   1) Interop.IWshRuntimeLibrary
+    ///   2) Interop.Shell32
+    ///   
+    /// Then open up the properties of both references and change the property "Embed Interop-types" to false!!!
+    /// </remarks>
+    public class AutostartFolder
+    {
+        #region ------------- Properties ----------------------------------------------------------
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        #endregion
+
+
+
+        #region ------------- Init ----------------------------------------------------------------
+        public AutostartFolder()
+        {
+            ProductName = "MyProduct";
+            Description = "Launch My Application";
+        }
+
+        public AutostartFolder(string productName, string description)
+        {
+            ProductName = productName;
+            Description = description;
+        }
+        #endregion
+
+
+
+        #region ------------- Methods -------------------------------------------------------------
+        
+        /// <summary>
+        /// Create a new shortcut in windows autostart folder, pointing to your program.
+        /// The next time windows starts it will execute your program.
+        /// </summary>
+        /// 
+        /// <param name="pathToMyExe">
+        /// The full path to your program, i.e. C:\Program Files\MyCompany\MyApplication.exe
+        /// If you pass null it will take the filename of the current process.
+        /// </param>
+        /// 
+        /// <param name="appIconLocation">
+        /// Optional path to you application icon, i.e. C:\Program Files\MyCompany\MyApplication.ico
+        /// </param>
+        public void CreateShortcut(string pathToMyExe = null, string appIconLocation = null)
+        {
+            WshShellClass wshShell = new WshShellClass();
+            IWshRuntimeLibrary.IWshShortcut shortcut;
+            string startUpFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+
+            // Create the shortcut
+            string Dateiname = startUpFolderPath + "\\" + ProductName + ".lnk";
+            shortcut = (IWshRuntimeLibrary.IWshShortcut)wshShell.CreateShortcut(Dateiname);
+
+            if (pathToMyExe == null)
+                pathToMyExe = GetTargetExeName();
+            shortcut.TargetPath = pathToMyExe;
+            shortcut.WorkingDirectory = Path.GetDirectoryName(shortcut.TargetPath);
+            shortcut.Description = Description;
+
+            if (appIconLocation != null)
+                shortcut.IconLocation = appIconLocation;
+            shortcut.Save();
+        }
+
+        /// <summary>
+        /// Removes all shortcuts from windows autostart folder that point to you program.
+        /// </summary>
+        /// 
+        /// <param name="pathToMyExe">
+        /// The full path to your program, i.e. C:\Program Files\MyCompany\MyApplication.exe
+        /// If you pass null it will take the filename of the current process.
+        /// </param>
+        public void DeleteShortcut(string pathToMyExe = null)
+        {
+            if (pathToMyExe == null)
+                pathToMyExe = GetTargetExeName();
+
+            string startUpFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+
+            DirectoryInfo di = new DirectoryInfo(startUpFolderPath);
+            FileInfo[] files = di.GetFiles("*.lnk");
+
+            foreach (FileInfo fi in files)
+            {
+                string shortcutTargetFile = GetTargetFilenameByShortcutFilename(fi.FullName);
+
+                if (shortcutTargetFile.EndsWith(pathToMyExe, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    System.IO.File.Delete(fi.FullName);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get the target filename by the name of the shortcut in windows autostart folder.
+        /// </summary>
+        /// 
+        /// <param name="pathToMyExe">
+        /// The full path to the shortcut file, i.e. C:\Users\__YOUR_USERNAME__\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\notepad++.lnk
+        /// </param>
+        public string GetTargetFilenameByShortcutFilename(string shortcutFilename)
+        {
+            string pathOnly = Path.GetDirectoryName(shortcutFilename);
+            string filenameOnly = Path.GetFileName(shortcutFilename);
+
+            Shell32.Shell shell = new Shell32.ShellClass();
+            Shell32.Folder folder = shell.NameSpace(pathOnly);
+            Shell32.FolderItem folderItem = folder.ParseName(filenameOnly);
+            if (folderItem != null)
+            {
+                Shell32.ShellLinkObject link = (Shell32.ShellLinkObject)folderItem.GetLink;
+                return link.Path;
+            }
+
+            return String.Empty;
+        }
+        #endregion
+
+
+
+        #region ------------- Implementation ------------------------------------------------------
+        private static string GetTargetExeName()
+        {
+            string ExeName = Environment.GetCommandLineArgs()[0];
+            if (ExeName.Contains(".vshost.exe"))
+                ExeName = ExeName.Replace(".vshost.exe", ".exe");
+            return ExeName;
+        }
+        #endregion
+    }
+}
---
+++ ExternalPrograms.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+
+namespace Abraham.Windows.Shell
+{
+    /// <summary>
+    /// Provides methods to open file with the associated program, associated browser or to start a batch file.
+    /// </summary>
+    ///
+    /// <remarks>
+    /// Oliver Abraham, www.oliver-abraham.de, mail@oliver-abraham.de
+    /// </remarks>
+    public class ExternalPrograms
+    {
+        #region ------------- Methods -------------------------------------------------------------
+
+        /// <summary>
+        /// Starts an external process with the given filename and arguments
+        /// </summary>
+        public static void StartProcess(string program, string arguments)
+        {
+            Process.Start(program, arguments);
+        }
+
+        /// <summary>
+        /// Starts a batchfile in cmd.exe or powershell.exe interpreter
+        /// </summary>
+        public static void StartBatchfile(string fullPathToBatchfile)
+		{
+			string currentDirectory = Directory.GetCurrentDirectory();
+			StartBatchfile_internal(fullPathToBatchfile);
+			Directory.SetCurrentDirectory(currentDirectory);
+		}
+
+        /// <summary>
+        /// Opens up a windows explorer with the given directory
+        /// </summary>
+		public static void OpenDirectoryInExplorer(string directoryName)
+        {
+            if (Directory.Exists(directoryName) == false)
+                throw new Exception("path does not exist");
+
+            string Programm = "explorer.exe";
+            Process.Start(Programm, " \"" + directoryName + "\"");
+        }
+
+        /// <summary>
+        /// Opens a file with the associated browser
+        /// </summary>
+        public static void OpenFileInStandardBrowser(string filename)
+        {
+            var extension = Path.GetExtension(filename);
+            string program = FindAssociatedProgramFor(extension);
+            if (program == null)
+                return;
+
+            Process.Start(program, " \"" + filename + "\"");
+        }
+
+        /// <summary>
+        /// Opens a file with the associated editor
+        /// </summary>
+        public static void OpenFileInAssociatedEditor(string filename)
+        {
+            var extension = Path.GetExtension(filename);
+            string program = FindAssociatedProgramFor(extension);
+            if (program == null)
+                return;
+
+            bool bracketsAreAlreadyThere = (program.Contains("\"%1\"") || program.Contains("\" %1\""));
+            string filenameInBrackets = bracketsAreAlreadyThere 
+                ? filename
+                : $"\"{filename}\"";
+
+            var parts = program.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            if (parts.GetLength(0) > 0)
+			{
+                var exeFile = parts[0];
+                Process.Start(exeFile, filenameInBrackets);
+			}
+        }
+
+        /// <summary>
+        /// Looks up the associated program for a given file extension
+        /// </summary>
+		public static string FindAssociatedProgramFor(string extension)
+		{
+            var docName = Registry.GetValue($@"HKEY_CLASSES_ROOT\{extension}", string.Empty, string.Empty) as string;
+            if (!string.IsNullOrEmpty(docName))
+            {
+                var associatedProgram = Registry.GetValue($@"HKEY_CLASSES_ROOT\{docName}\shell\open\command", string.Empty, string.Empty) as string;
+                return associatedProgram;
+            }
+            return null;
+        }
+		#endregion
+
+
+
+		#region ------------- Implementation ------------------------------------------------------
+		private static void StartBatchfile_internal(string fullPathToBatchfile)
+		{
+			string workingDirectory = Path.GetPathRoot(fullPathToBatchfile);
+			Directory.SetCurrentDirectory(workingDirectory);
+
+            var extension = Path.GetExtension(fullPathToBatchfile);
+
+            var fileIsAPowershellBatch = extension.EndsWith("ps1");
+            var fileIsACmdBatch = extension.EndsWith("cmd") || extension.EndsWith("bat");
+
+            var commandShell = fileIsAPowershellBatch 
+                ? "powershell.exe"
+                : fileIsACmdBatch 
+                    ? "cmd.exe"
+                    : throw new Exception($"unsupported file type '{extension}'");
+
+			Process.Start(commandShell, " /k \"" + fullPathToBatchfile + "\"");
+		}
+		#endregion
+	}
+}
